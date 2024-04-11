e4c720c04c85ed51e7328bf0708c3cc4e58b526f
azamat-aminov <azamatmaqsudjonovich@gmail.com>
2024-04-11 12:32:26 +0500

    decorator pattern(edited)

--- src/main/java/com/design/decorator/StarbuzzCoffee.java
+++ src/main/java/com/design/decorator/StarbuzzCoffee.java
@@ -1,8 +1,5 @@
 package com.design.decorator;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-
 public class StarbuzzCoffee {
 
     public static void main(String[] args) {
@@ -14,8 +11,5 @@ public class StarbuzzCoffee {
         beverage2 = new Mocha(beverage2);
         beverage2 = new Whip(beverage2);
         System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
-
-        FileInputStream fileInputStream;
-        BufferedInputStream bufferedInputStream;
     }
 }
